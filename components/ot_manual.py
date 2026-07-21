dd3f6798247855338541de3de27528b837a03364
selene-mvvn <lekhanhhang147@gmail.com>
2026-07-21 10:04:41 +0700

    Fix

--- components/ot_manual.py
+++ components/ot_manual.py
@@ -558,10 +558,12 @@ def render_base_data():
             }}
             </style>
             <div id="gross-tooltip" style="position: relative; width: 100%; height: 0px; z-index: 999; pointer-events: none;">
-                <div class="st-tooltip-icon">
-                    <svg viewBox="0 0 24 24" width="17" height="17" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z"/>
-                    </svg>
+                <div class="st-tooltip-wrapper">
+                    <div class="st-tooltip-icon">
+                        <svg viewBox="0 0 24 24" width="17" height="17" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M11 18h2v-2h-2v2zm1-16C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm0-14c-2.21 0-4 1.79-4 4h2c0-1.1.9-2 2-2s2 .9 2 2c0 2-3 1.75-3 5h2c0-2.25 3-2.5 3-5 0-2.21-1.79-4-4-4z"/>
+                        </svg>
+                    </div>
                     <div class="st-tooltip-text">
                         {t('Lưu ý: Cột Lương Gross sẽ được tính TỰ ĐỘNG khi bạn bấm Lưu.', '注:「総支給額」は保存時に自動計算されます。')}
                     </div>
